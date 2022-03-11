469846e20b30f52ac0665194228079c72550cdbf
Thagana <samuelmthwa79@gmail>
2022-03-11 22:26:17 +0200

    small update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,18 @@
 import styles from '../styles/Home.module.css'
+import useWindow from 'react-use/lib/useWindowSize';
+import Confetti from 'react-confetti';
 
 export default function Home() {
+  const { width, height } = useWindow();
   return (
     <div className={styles.container}>
-      Coming Soon
+      <Confetti
+        width={width}
+        height={height}
+      />
+      <div className={styles.comingText}>
+        Coming Soon
+      </div>
     </div>
   )
 }
