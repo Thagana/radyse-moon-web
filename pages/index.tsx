f88afbdd2512380c57f5c049d25bd214245ac13a
Thagana <samuelmthwa79@gmail>
2022-03-13 16:01:31 +0200

    remved confetti

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,11 @@
 import styles from '../styles/Home.module.css'
 import useWindow from 'react-use/lib/useWindowSize';
-import Confetti from 'react-confetti';
 
 export default function Home() {
   const { width, height } = useWindow();
   return (
     <div className={styles.container}>
-      <Confetti
-        width={width}
-        height={height}
-      />
-      <div className={styles.comingText}>
-        Coming Soon
-      </div>
+      <div className={styles.comingText}>Coming Soon!</div>
     </div>
   )
 }
