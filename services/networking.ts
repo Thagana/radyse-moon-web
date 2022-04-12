f98576abc2e934aa11a0045b1cac876b9e5a070b
Thagana <samuelmthwa79@gmail>
2022-04-12 22:26:32 +0200

    contact us

---
+++ services/networking.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import {config} from '../config/configs';
+
+export default class Remote {
+    static async sendContactForm(details: { name: string, email: string, message: string }) {
+        try {
+            const response = await axios.post(`${config.SERVER_URL}/contact-form`, {
+                ...details
+            });
+            if (response.status === 200) {
+                return {
+                    success: true,
+                    data: response.data.message
+                }
+            }
+            return {
+                success: false,
+                message: 'Something went wrong, please try again later'
+            }
+        } catch (error) {
+            return {
+                success: false,
+                message: 'Something went wrong, please try again later'
+            }
+        }
+    }
+}
