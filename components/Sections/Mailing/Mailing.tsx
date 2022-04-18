b602d0ee3f92743bf3f02f8b3a0b26d3b3024415
Thagana <samuelmthwa79@gmail>
2022-04-18 18:58:40 +0200

    mailing list update

--- components/Sections/Mailing/Mailing.tsx
+++ components/Sections/Mailing/Mailing.tsx
@@ -1,28 +1,60 @@
-import React from 'react'
-import Button from '../../common/Button'
+import React from "react";
+import Button from "../../common/Button";
+
+import Network from '../../../services/networking';
 
 export default function Mailing() {
+  const [email, setEmail] = React.useState('');
+  const [serverMessage, setServerMessage] = React.useState('');
+  const [loading, setLoading] = React.useState(false);
+
+  const handleSubmit = async (event: React.SyntheticEvent) => {
+    try {
+      setLoading(true);
+      event.preventDefault();
+      const response = await Network.sendMailListUpdate(email);
+      setServerMessage(response.message);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setServerMessage('Something went wrong, please try again later');
+      setLoading(false);
+    }
+  };
+
   return (
-    <div className="mailing-list">
-    <div className='header'>
+    <div className='mailing-list'>
+      <div>
+          {serverMessage && (
+              <div className={`alert alert-${serverMessage ? 'danger': 'primary'}`} role="alert">
+                  {serverMessage}
+              </div>
+          )}
+      </div>
+      <div className='header'>
         <header>
-            <h3>
-                Get notified when we have updates
-            </h3>
+          <h3>Get notified when we have updates</h3>
         </header>
+      </div>
+      <div className='email-form'>
+        <form className='form' onSubmit={handleSubmit}>
+          <div className='form-group'>
+            <input
+              name='email'
+              type='email'
+              value={email}
+              onChange={(val) => setEmail(val.target.value)}
+              placeholder='Enter Email'
+              className='form-control'
+            />
+          </div>
+          <div className='form-group'>
+            <Button design='primary' type='submit'>
+              {loading ? 'Loading ...' : 'Submit' } 
+            </Button>
+          </div>
+        </form>
+      </div>
     </div>
-    <div className="email-form">
-      <form className='form'>
-        <div className="form-group">
-          <input name="email" type="email" placeholder="Enter Email" className="form-control" />
-        </div>
-        <div className='form-group'>
-          <Button onClick={() => {}} design="primary">
-            Send
-          </Button>
-        </div>
-      </form>
-    </div>
-  </div>
-  )
+  );
 }
--- services/networking.ts
+++ services/networking.ts
@@ -24,4 +24,26 @@ export default class Remote {
             }
         }
     }
+    static async sendMailListUpdate(email: string) {
+        try {
+            const response = await axios.post(`${config.SERVER_URL}/contact-form/mailing`, {
+                email
+            })
+            if (response.status === 200) {
+                return {
+                    success: true,
+                    data: response.data.message
+                }
+            }
+            return {
+                success: false,
+                message: 'Something went wrong, please try again later'
+            }
+        } catch (error) {
+            return {
+                success: false,
+                message: 'Something went wrong, please try again later'
+            }
+        }
+    }
 }
