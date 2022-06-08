16f0f78284692ef8d9224cfd3c49720c76a5e05b
Thagana <samuelmthwa79@gmail>
2022-06-08 21:56:06 +0200

    google tracking code - added

--- components/Meta/Meta.tsx
+++ components/Meta/Meta.tsx
@@ -1,28 +1,44 @@
-import React from 'react'
-import Head from 'next/head';
+import React from "react";
+import Head from "next/head";
 
 interface Props {
-    keywords: string;
-    title: string;
-    description: string;
+  keywords: string;
+  title: string;
+  description: string;
 }
 
 export default function Meta(props: Props) {
-    const { keywords, title, description } = props;
+  const { keywords, title, description } = props;
   return (
     <Head>
-    <title>{title}</title>
-    <meta property="og:title" content="OvalBark" key="title" />
-    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    <meta name="description" content={description} />
-    <meta name="name" content="OvalBark" />
-    <meta name="keywords" content={keywords} />
-  </Head>
-  )
+      <title>{title}</title>
+      <meta property='og:title' content='OvalBark' key='title' />
+      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      <meta name='description' content={description} />
+      <meta name='name' content='OvalBark' />
+      <meta name='keywords' content={keywords} />
+      <script
+        async
+        src='https://www.googletagmanager.com/gtag/js?id=G-B92XVTGRKR'
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-B92XVTGRKR');
+        `,
+        }}
+      />
+    </Head>
+  );
 }
 
 Meta.defaultProps = {
-    title: 'OvalBark',
-    keywords: 'News, Updates, Breaking News, Application, OvalBark, News Subscription',
-    description: "OvalBark is a news application that delivers news articles to you"
-}
+  title: "OvalBark",
+  keywords:
+    "News, Updates, Breaking News, Application, OvalBark, News Subscription",
+  description:
+    "OvalBark is a news application that delivers news articles to you",
+};
