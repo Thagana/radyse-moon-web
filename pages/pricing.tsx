92064aa04a7fca45501ddfb4e2caaba65cebc423
Thagana <samuelmthwa79@gmail>
2022-06-16 17:58:29 +0200

    fixing pricing pages

--- pages/pricing.tsx
+++ pages/pricing.tsx
@@ -6,6 +6,12 @@ import {config} from '../config/configs';
 
 export default function pricing() {
   const handleClick = (subType: string) => {
+    if (subType === 'Free') {
+      window.open(
+        `${config.APP_URL}/login`,
+        "_blank"
+      );
+    }
     window.open(
       `${config.APP_URL}/login?redirect=subscribe_${subType}`,
       "_blank"
@@ -22,6 +28,35 @@ export default function pricing() {
       </div>
       <div className='subscription'>
         <div className='cardWrap'>
+        <motion.div
+            whileHover={{
+              scale: 1.1,
+              transition: { duration: 0.2 },
+            }}
+            className='pricingCard'
+          >
+            <p className='planName'>Free Access</p>
+            <p className='planPriceDescription'>
+              A free access with the basic features
+            </p>
+            <div className='priceDiv'>$0 monthly</div>
+            <p className='whatsIncluded'>What's included:</p>
+            <ul>
+              <li>View news article from difference sources</li>
+              <li>Bookmark news for offline viewing</li>
+              <li>Get news based on viewing patterns</li>
+            </ul>
+            <div>
+              <Button
+                design='primary long'
+                onClick={() => {
+                  handleClick("Free");
+                }}
+              >
+                Sign Up
+              </Button>
+            </div>
+          </motion.div>
           <motion.div
             whileHover={{
               scale: 1.1,
@@ -33,12 +68,12 @@ export default function pricing() {
             <p className='planPriceDescription'>
               A starter plan for getting up to date with the platform
             </p>
-            <div className='priceDiv'>$2 monthly</div>
+            <div className='priceDiv'>$15 monthly</div>
             <p className='whatsIncluded'>What's included:</p>
             <ul>
-              <li>Up to 2 Email News updates per/day</li>
-              <li>Up to 2 Mobile Push News updates per/day</li>
-              <li>Up to 2 SMS Push News updates per/day</li>
+              <li>Up to 4 Email News updates per/day</li>
+              <li>Up to 4 Mobile Push News updates per/day</li>
+              <li>Up to 4 SMS Push News updates per/day</li>
             </ul>
             <div>
               <Button
@@ -60,44 +95,14 @@ export default function pricing() {
           >
             <p className='planName'>Fanatic</p>
             <p className='planPriceDescription'>
-              More features than the Intrigued plan
+              Plus features than the Intrigued plan
             </p>
-            <div className='priceDiv'>$ 3 monthly</div>
+            <div className='priceDiv'>$ 30 monthly</div>
             <p className='whatsIncluded'>What's included:</p>
             <ul>
-              <li>Up to 4 Mobile Push News updates per/day</li>
-              <li>Up to 4 SMS Push News updates per/day</li>
+              <li>Unlimited Mobile Push News updates per/day</li>
+              <li>Unlimited SMS Push News updates per/day</li>
               <li>News Analytics (Compare news articles)</li>
-            </ul>
-            <div>
-              <Button
-                design='primary long'
-                onClick={() => {
-                  handleClick("Fanatic");
-                }}
-              >
-                Subscribe
-              </Button>
-            </div>
-          </motion.div>
-          <motion.div
-            whileHover={{
-              scale: 1.1,
-              transition: { duration: 0.2 },
-            }}
-            className='pricingCard'
-          >
-            <p className='planName'>In The Know</p>
-            <p className='planPriceDescription'>
-              All plus News Settings and Analytics
-            </p>
-            <div className='priceDiv'>$ 10 monthly</div>
-            <p className='whatsIncluded'>What's included:</p>
-            <ul>
-              <li>Email Notification as they come</li>
-              <li>SMS Notification as they come</li>
-              <li>Mobile Push Notification as they come</li>
-              <li>Get News Analytics</li>
             </ul>
             <div>
               <Button
